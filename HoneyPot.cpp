fac960b46fa73b7a025e69c6b436dcee0e1f79ce
kooshakooshadv <kooshakooshadv@gmail.com>
2022-04-25 01:56:26 +0430

    Bug Fixed from Directories

--- HoneyPot.cpp
+++ HoneyPot.cpp
@@ -53,7 +53,7 @@ int main(){
 		}else if( (command == "cd /bin") || (command == "cd /bin/")){
 			while(true){
 				string bin_command;
-				cout << "[root@MyServer]/# ";
+				cout << "[root@MyServer]/bin# ";
 				getline(cin,bin_command);
 				if (bin_command == "pwd"){
 					cout << "/bin" << endl;
@@ -67,7 +67,7 @@ int main(){
 		}else if( (command == "cd /etc") || (command == "cd /etc/")){
 			while(true){
 				string etc_command;
-				cout << "[root@MyServer]/# ";
+				cout << "[root@MyServer]/etc# ";
 				getline(cin,etc_command);
 				if (etc_command == "pwd"){
 					cout << "/etc" << endl;
@@ -81,7 +81,7 @@ int main(){
 		}else if( (command == "cd /lib") || (command == "cd /lib/")){
 			while(true){
 				string lib_command;
-				cout << "[root@MyServer]/# ";
+				cout << "[root@MyServer]/lib# ";
 				getline(cin,lib_command);
 				if (lib_command == "pwd"){
 					cout << "/lib" << endl;
@@ -95,7 +95,7 @@ int main(){
 		}else if( (command == "cd /mnt") || (command == "cd /mnt/")){
 			while(true){
 				string mnt_command;
-				cout << "[root@MyServer]/# ";
+				cout << "[root@MyServer]/mnt# ";
 				getline(cin,mnt_command);
 				if (mnt_command == "pwd"){
 					cout << "/mnt" << endl;
@@ -110,7 +110,7 @@ int main(){
 		}else if( (command == "cd /opt") || (command == "cd /opt/")){
 			while(true){
 				string opt_command;
-				cout << "[root@MyServer]/# ";
+				cout << "[root@MyServer]/opt# ";
 				getline(cin,opt_command);
 				if (opt_command == "pwd"){
 					cout << "/opt" << endl;
@@ -124,7 +124,7 @@ int main(){
 		}else if( (command == "cd /tmp") || (command == "cd /tmp/")){
 			while(true){
 				string tmp_command;
-				cout << "[root@MyServer]/# ";
+				cout << "[root@MyServer]/tmp# ";
 				getline(cin,tmp_command);
 				if (tmp_command == "pwd"){
 					cout << "/etc" << endl;
@@ -138,7 +138,7 @@ int main(){
 		}else if( (command == "cd /home") || (command == "cd /home/")){
 			while(true){
 				string home_command;
-				cout << "[root@MyServer]/# ";
+				cout << "[root@MyServer]/home# ";
 				getline(cin,home_command);
 				if (home_command == "pwd"){
 					cout << "/home" << endl;
@@ -152,7 +152,7 @@ int main(){
 		}else if( (command == "cd /lib64") || (command == "cd /lib64/")){
 			while(true){
 				string lib64_command;
-				cout << "[root@MyServer]/# ";
+				cout << "[root@MyServer]/lib64# ";
 				getline(cin,lib64_command);
 				if (lib64_command == "pwd"){
 					cout << "/lib64" << endl;
@@ -166,7 +166,7 @@ int main(){
 		}else if( (command == "cd /media") || (command == "cd /media/")){
 			while(true){
 				string media_command;
-				cout << "[root@MyServer]/# ";
+				cout << "[root@MyServer]/media# ";
 				getline(cin,media_command);
 				if (media_command == "pwd"){
 					cout << "/media" << endl;
@@ -180,7 +180,7 @@ int main(){
 		}else if( (command == "cd /sbin") || (command == "cd /sbin/")){
 			while(true){
 				string sbin_command;
-				cout << "[root@MyServer]/# ";
+				cout << "[root@MyServer]/sbin# ";
 				getline(cin,sbin_command);
 				if (sbin_command == "pwd"){
 					cout << "/sbin" << endl;
@@ -194,7 +194,7 @@ int main(){
 		}else if( (command == "cd /sys") || (command == "cd /sys/")){
 			while(true){
 				string sys_command;
-				cout << "[root@MyServer]/# ";
+				cout << "[root@MyServer]/sys# ";
 				getline(cin,sys_command);
 				if (sys_command == "pwd"){
 					cout << "/sys" << endl;
@@ -208,7 +208,7 @@ int main(){
 		}else if( (command == "cd /usr") || (command == "cd /usr/")){
 			while(true){
 				string usr_command;
-				cout << "[root@MyServer]/# ";
+				cout << "[root@MyServer]/usr# ";
 				getline(cin,usr_command);
 				if (usr_command == "pwd"){
 					cout << "/usr" << endl;
@@ -222,7 +222,7 @@ int main(){
 		}else if( (command == "cd /proc") || (command == "cd /proc/")){
 			while(true){
 				string proc_command;
-				cout << "[root@MyServer]/# ";
+				cout << "[root@MyServer]/proc# ";
 				getline(cin,proc_command);
 				if (proc_command == "pwd"){
 					cout << "/proc" << endl;
